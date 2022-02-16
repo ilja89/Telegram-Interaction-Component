6727abe131c3f8c4b678315987ae73eaacd77838
ilja89 <52709138+ilja89@users.noreply.github.com>
2022-02-16 21:06:02 +0200

    Add files via upload

--- Telegram Interaction Component/Form1.vb
+++ Telegram Interaction Component/Form1.vb
@@ -1,17 +1,21 @@
 ﻿Imports TelegramBotLib
 Public Class Form1
     Private telecom As New TelegramBotLib.CTBot
+    Private cObjects As New Collection
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Send.Click
         RichTextBox1.Text = telecom.SendTelegramMessage(RichTextBoxInput.Text).Text
     End Sub
 
     Private Sub GetUpdates_Click(sender As Object, e As EventArgs) Handles GetUpdates.Click
-        Dim a As New telResponse
-        a.SetError = "error"
-        If (RichTextBoxInput.Text = "") Then
-            RichTextBox1.Text = telecom.GetUpdate().Text
-        Else
-            RichTextBox1.Text = telecom.GetUpdate(CInt(RichTextBoxInput.Text)).Text
-        End If
+        RichTextBox1.Text = telecom.GetUpdate().Text
+    End Sub
+
+    Function test()
+
+    End Function
+
+    Sub Loaded() Handles Me.Load
+        Dim cObject As New TelegramBotLib.CommandObject(AddressOf test, "/help")
+        cObjects.Add()
     End Sub
 End Class
