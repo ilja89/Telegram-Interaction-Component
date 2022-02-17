afa672ba4829d7530a8c93ca500904b8c0c10a0d
ilja89 <52709138+ilja89@users.noreply.github.com>
2022-02-17 03:01:18 +0200

    Added new class for linking together Telegram command T and any custom function X existing in any environment E.

--- Telegram Interaction Component/Form1.vb
+++ Telegram Interaction Component/Form1.vb
@@ -1,7 +1,7 @@
 ﻿Imports TelegramBotLib
 Public Class Form1
     Private telecom As New TelegramBotLib.CTBot
-    Private cObjects As New Collection
+    Private cObjCollection As New Collection
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Send.Click
         RichTextBox1.Text = telecom.SendTelegramMessage(RichTextBoxInput.Text).Text
     End Sub
@@ -10,12 +10,19 @@ Public Class Form1
         RichTextBox1.Text = telecom.GetUpdate().Text
     End Sub
 
-    Function test()
-
+    Function test(cObject As Object)
+        RichTextBox1.Text = "Delegated Function" + vbCrLf +
+        cObject.Command.ToString + vbCrLf +
+        cObject.FunctionCallType.ToString + vbCrLf +
+        cObject.FunctionName.ToString + vbCrLf +
+        cObject.IsCreated.ToString + vbCrLf +
+        cObject.ObjectRef.ToString()
+        Return 111
     End Function
 
     Sub Loaded() Handles Me.Load
-        Dim cObject As New TelegramBotLib.CommandObject(AddressOf test, "/help")
-        cObjects.Add()
+        Dim cObject As New TelegramBotLib.commandObject("/help", Me, "test", vbMethod)
+        cObjCollection.Add(cObject, cObject.Command)
+        cObjCollection.Item("/help").CallCommand(cObjCollection.Item("/help"))
     End Sub
 End Class
--- TelegramBotLib/CTBot.vb
+++ TelegramBotLib/CTBot.vb
@@ -120,6 +120,91 @@ Public Class CTBot
     End Function
 End Class
 
+' This class is used to link together any custom Telegram command and function, what handles it
+' Using this object function with name "commandObjectFunctionName" what exists in environment
+' "commandObjectObjectRef" (Class, for example) can be called with ease using this object function
+' "CallCommand", what gets array of optional parameters, and returns result of called function with
+' "commandObjectFunctionName" name. 
+Public Class commandObject
+    Private commandObjectObjectRef As Object = Nothing          ' Environment inside what aim function will be searched
+    Private commandObjectFunctionName As String = Nothing       ' Aim function name
+    Private commandObjectFunctionCallType As CallType = Nothing ' Aim function call type
+    Private commandObjectCommand As String = Nothing            ' A Telegram command, to what this function is related
+    Private commandObjectCreated As Boolean = False             ' Shows if object is correctly created
+    Public ReadOnly Property Command As String
+        Get
+            If (commandObjectCreated) Then
+                Return commandObjectCommand
+            Else
+                Return Nothing
+            End If
+        End Get
+    End Property
+    Public ReadOnly Property IsCreated As Boolean
+        Get
+            If (commandObjectCreated) Then
+                Return True
+            Else
+                Return False
+            End If
+        End Get
+    End Property
+    Public ReadOnly Property ObjectRef As Object
+        Get
+            If (commandObjectCreated) Then
+                Return commandObjectObjectRef
+            Else
+                Return Nothing
+            End If
+        End Get
+    End Property
+    Public ReadOnly Property FunctionName As String
+        Get
+            If (commandObjectCreated) Then
+                Return commandObjectFunctionName
+            Else
+                Return Nothing
+            End If
+        End Get
+    End Property
+    Public ReadOnly Property FunctionCallType As CallType
+        Get
+            If (commandObjectCreated) Then
+                Return commandObjectFunctionCallType
+            Else
+                Return Nothing
+            End If
+        End Get
+    End Property
+    Public Sub New(
+                  ByVal constructorCommand As String,
+                  ByRef constructorObjectRef As Object,
+                  ByVal constructorFunctionName As String,
+                  ByVal constructorFunctionCallType As CallType)
+        If (
+        constructorCommand IsNot Nothing And
+        constructorObjectRef IsNot Nothing And
+        constructorFunctionName IsNot Nothing And
+        constructorFunctionCallType <> Nothing
+        ) Then
+            commandObjectCommand = constructorCommand
+            commandObjectObjectRef = constructorObjectRef
+            commandObjectFunctionCallType = constructorFunctionCallType
+            commandObjectFunctionName = constructorFunctionName
+            commandObjectCreated = True
+        Else
+            Exit Sub
+        End If
+    End Sub
+    Public Function CallCommand(ParamArray Args As Object())
+        If (commandObjectCreated) Then
+            Return CallByName(ObjectRef, FunctionName, FunctionCallType, Args)
+        Else
+            Return Nothing
+        End If
+    End Function
+End Class
+
 Public Class telResponse
     Private responseText As String = ""             ' Response text
     Private responseCommand As String = ""          ' Response command
